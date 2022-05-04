a8dff58e48f1a62ec95e2586b75651ea94877e49
12ArchanaA <archana8579@gmail.com>
2022-05-04 15:50:41 +0530

    commit

--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -1,4 +1,5 @@
 import "./Notifications.css"
+import Image from "./../Assets/images/dpChat.svg" 
 const Notifications = () => {
     return (
         <div className="notifications-container">
@@ -6,6 +7,54 @@ const Notifications = () => {
                 <h3>Notifications</h3>
             </div>
             <div className="notifications-content">
+
+                <div className="users">
+                    <div className="notification-dp-image">
+                        <img src={Image} alt="" />
+                    </div>
+                    <div className="user-name-para">
+                        <p>new user started following you</p>
+                    </div>
+                    <div className="time">
+                        <p>1 hr ago</p>
+                    </div>
+                </div>
+
+                <div className="users">
+                    <div className="notification-dp-image">
+                        <img src={Image} alt="" />
+                    </div>
+                    <div className="user-name-para">
+                        <p>new user started following you</p>
+                    </div>
+                    <div className="time">
+                        <p>1 hr ago</p>
+                    </div>
+                </div>
+
+                <div className="users">
+                    <div className="notification-dp-image">
+                        <img src={Image} alt="" />
+                    </div>
+                    <div className="user-name-para">
+                        <p>new user started following you</p>
+                    </div>
+                    <div className="time">
+                        <p>1 hr ago</p>
+                    </div>
+                </div>
+
+                <div className="users">
+                    <div className="notification-dp-image">
+                        <img src={Image} alt="" />
+                    </div>
+                    <div className="user-name-para">
+                        <p>new user started following you</p>
+                    </div>
+                    <div className="time">
+                        <p>1 hr ago</p>
+                    </div>
+                </div>
 
             </div>
         </div>
--- src/Components/StoryViews.jsx
+++ src/Components/StoryViews.jsx
@@ -1,6 +1,6 @@
 import "./StoryViews.css"
 import Eye from "./../Assets/images/viewEye.svg"
-import Share from "./../Assets/images/viewShare.svg"
+// import Share from "./../Assets/images/viewShare.svg"
 import Dustbin from "./../Assets/images/viewDustbin.svg"
 import DpPicture from "./../Assets/images/dpChat.svg"
 
@@ -11,9 +11,9 @@ const StoryViews = () => {
                 <div className="upper-part-division">
                     <img src={Eye} alt="" />
                 </div>
-
-                <div className="upper-part-division">
-                    <img src={Share} alt="" />
+                
+                 <div className="upper-part-division">
+                    <h3>100</h3>
                 </div>
 
                 <div className="upper-part-division">
